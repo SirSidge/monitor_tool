b37824118bab11ef7db55054dd7c33c744b3f53a
Heinz <heinz.hscharlach@gmail.com>
2025-12-24 23:23:05 +0200

    Added counters for time spent

--- GUI.py
+++ GUI.py
@@ -4,6 +4,9 @@ import psutil
 from pystray import Icon, Menu, MenuItem
 from PIL import Image
 import time
+import json
+import ctypes
+from ctypes import wintypes
 
 class MonitorToolUI:
     _VALID_STATES = {"productive", "idle", "unproductive"}
@@ -11,7 +14,7 @@ class MonitorToolUI:
     def __init__(self, minimize_on_start=False):
         self.root = tk.Tk()
         self.root.protocol("WM_SAVE_YOURSELF", self.log_shutdown)
-        self.root.geometry("800x580")
+        self.root.geometry("800x640") #Window size
         self.root.title("Monitor Tool")
         self.prev_state = False
         self.root.withdraw()
@@ -21,6 +24,12 @@ class MonitorToolUI:
         self.productivity_status = "idle"
         self.updating_processes = False
         self._last_process_update = 0
+        
+        self.temp_stats = {"Day": 0, "Week": 12, "Month": 48, "Total": 210}
+        self.h_day = self.temp_stats["Day"] # Need to pull info from the above temp_stats
+        self.h_week = self.temp_stats["Week"]
+        self.h_month = self.temp_stats["Month"]
+        self.h_total = self.temp_stats["Total"]
 
         style = ttk.Style()
         style.configure("Big.TLabel", font=("Helvetica", 24))
@@ -37,6 +46,10 @@ class MonitorToolUI:
 
         self.timer_label = ttk.Label(self.root, text="", font=("Helvetica", 14), foreground="red", anchor="e")
         self.timer_label.pack(fill="x", padx=50, pady=(0, 20))
+
+        # Stats
+        self.stats_label = ttk.Label(self.root, text="Day:\nWeek:\nMonth:\nTotal:", font=("Helvetica", 14), anchor="nw")
+        self.stats_label.pack(fill="x", padx=50, pady=(0, 20))
 
         # Scrollable list-box for currently running process names
         header = ttk.Label(self.root, text="Running process names (for debugging):", font=("Helvetica", 12))
@@ -70,10 +83,25 @@ class MonitorToolUI:
         self.root.protocol("WM_DELETE_WINDOW", self.hide_window)
 
     def log_startup(self):
+        try:
+            with open(fr'C:\monitoring tool - temp data\stats.json', "r", encoding='utf-8') as f:
+                self.temp_stats = json.load(f)
+                self.h_day = self.temp_stats["Day"]
+                self.h_week = self.temp_stats["Week"]
+                self.h_month = self.temp_stats["Month"]
+                self.h_total = self.temp_stats["Total"]
+        except FileNotFoundError:
+            temp_data = {"Day": 0, "Week": 12, "Month": 48, "Total": 210}
+            with open(fr'C:\monitoring tool - temp data\stats.json', 'w') as f:
+                json.dump(temp_data, f, indent=4)
+            self.temp_stats = temp_data
         with open(self.get_file_path(), 'a', encoding='utf-8') as f:
             f.write(f"Monitoring started: {time.ctime()}\n")
     
     def log_shutdown(self):
+        with open(fr'C:\monitoring tool - temp data\stats.json', "w", encoding='utf-8') as f:
+            self.update_stats()
+            json.dump(self.temp_stats, f, indent=4)
         with open(self.get_file_path(), 'a', encoding='utf-8') as f:
             if self.prev_state:
                 f.write(f"end: {time.ctime()}\n")
@@ -83,6 +111,21 @@ class MonitorToolUI:
         if state not in self._VALID_STATES:
             raise ValueError(f"Invalid state: {state}")
         self.productivity_status = state
+
+    # Get foreground app
+    def get_foreground_process_name(self):
+        hwnd = ctypes.windll.user32.GetForegroundWindow()
+        if hwnd == 0:
+            return None
+        pid = wintypes.DWORD()
+        ctypes.windll.user32.GetWindowThreadProcessId(hwnd, ctypes.byref(pid))
+        if pid.value == 0:
+            return None
+        try:
+            process = psutil.Process(pid.value)
+            return process.name()
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            return None
 
     def update_process_list(self):
         self.process_listbox.delete(0, tk.END)
@@ -109,6 +152,18 @@ class MonitorToolUI:
             self._last_process_update = current_time
         self.root.after(1000, self.continue_process_updates)
 
+    def update_stats(self):
+        current_time = time.time()
+        if current_time - self._last_process_update >= 5:
+            self.h_day += 1
+            self.h_week += 2
+            self.h_month += 3
+            self.h_total += 4
+            self.temp_stats = {"Day": self.h_day, "Week": self.h_week, "Month": self.h_month, "Total": self.h_total}
+            self.stats_label.config(text=f"Day: {self.temp_stats["Day"]}\nWeek: {self.temp_stats["Week"]}\nMonth: {self.temp_stats["Month"]}\nTotal: {self.temp_stats["Total"]}")
+            self._last_process_update = current_time
+        self.root.after(1000, self.update_stats)
+
     def is_poe_running(self):
         return any(proc.name() == "PathOfExile.exe" for proc in psutil.process_iter(['name'])) # Note: Added the .lower(), if it suddenly breaks, this could be why.
     
@@ -133,8 +188,8 @@ class MonitorToolUI:
     def refresh(self):
         cpu_usage = psutil.cpu_percent(interval=None)
         self.foreground_label.config(text=f"Foreground App: TBC")
-
         self.cpu_label.config(text=f"CPU Usage: {cpu_usage:.1f}%")
+        self.update_stats()
 
         if self.is_poe_running():
             self._set_status("unproductive")
@@ -148,17 +203,25 @@ class MonitorToolUI:
                 self.start_time = time.time()
                 self.alarm = False
                 self.timer_label.config(text="")
-            self.poe_label.config(text=f"POE is running ⚠", foreground="red")
+            self.poe_label.config(text=f"Unproductive ⚠: {self.get_foreground_process_name()}", foreground="red")
             new_image = Image.new('RGB', (64, 64), 'red')
             self.icon.icon = new_image
-
             elapsed_time = time.time() - self.start_time
             if elapsed_time >= 7200 and not self.alarm: #7200 seconds
                 self.timer_label.config(text="2 hours depleted")
                 self.alarm = True
-
         elif self.is_vscode_running():
+            if self.prev_state:
+                try:
+                    with open(self.get_file_path(), 'a', encoding='utf-8') as f:
+                        f.write(f"end: {time.ctime()}\n")
+                except FileNotFoundError:
+                    print("The file does not exist.")
+                self.prev_state = False
+            new_image = Image.new('RGB', (64, 64), 'green')
+            self.icon.icon = new_image
             self._set_status("productive")
+            self.poe_label.config(text=f"Productive ✓: {self.get_foreground_process_name()}", foreground="green")
         else:
             self._set_status("idle")
             if self.prev_state:
@@ -168,8 +231,8 @@ class MonitorToolUI:
                 except FileNotFoundError:
                     print("The file does not exist.")
                 self.prev_state = False
-            self.poe_label.config(text=f"POE is not running ✓", foreground="green")
-            new_image = Image.new('RGB', (64, 64), 'green')
+            self.poe_label.config(text=f"Idle: {self.get_foreground_process_name()}", foreground="black")
+            new_image = Image.new('RGB', (64, 64), 'black')
             self.icon.icon = new_image
 
         self.root.after(1000, self.refresh)
--- README.md
+++ README.md
@@ -4,18 +4,28 @@
 1: Once elapsed has reached 2h (7200), it will keep displaying that 2h was reached regardless of the state of POE.
 
 # Features
-1: Unproductive calculator
+1: ----- Unproductive calculator ----- 
 1.1: Daily time limit of 2h
 1.2: Weekly time limit of 18h
 1.3: Reset and/or Sleep button for above timer.
 1.4: Auto-close unproductive apps
 
-2: Productivity calculator
+2: ----- Productivity calculator ----- 
 2.1: Measure productive time
 
-3: Find Apps
+3: ----- Find Apps ----- 
 3.1: Search for all apps currently running
 3.2: Add apps to 'productivity_status'
 
-4: Single app
+4: ----- Single monitoring app ----- 
 4.1: Check if app is already running, if so prevent second iteration.
+
+
+----- Misc -----
+What does the JSON file need to store?
+- Running totals: daily, weekly, monthly, and total
+- Time in statuses: Productive, Unproductive, Idle
+- What date and time events occured
+
+
+Can I have a C-code script running for my python app? I don't see how I can see what app is in the foreground using python, but maybe with C I can?
